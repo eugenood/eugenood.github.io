53081988622edf6a9c203e1664f435c3c5da6339
Eugene Lim <eugenood@gmail.com>
2025-08-05 09:16:41 +0800

    Create devcontainer.js

---
+++ .devcontainer/devcontainer.js
@@ -0,0 +1,11 @@
+{
+	"name": "Ubuntu",
+	"image": "mcr.microsoft.com/devcontainers/base:jammy",
+	"features": {
+		"ghcr.io/devcontainers/features/hugo:1": {
+			"extended": true
+		},
+	},
+	"forwardPorts": [1313],
+	"postCreateCommand": "git submodule update --init",
+}
